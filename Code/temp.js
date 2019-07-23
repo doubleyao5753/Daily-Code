8ce0f3a09e2ef8703f16cf54590a1920c1b238c8
doubleyao5753@gmail.com <47850689+OneShot5753@users.noreply.github.com>
2019-07-23 23:13:38 +0800

    add 03 - code

--- Code/temp.js
+++ Code/temp.js
@@ -1,5 +1,12 @@
-// Plan B:  concat() 
-function append0 (arr , item) {
-    return arr.concat(item)
-    // 牢记：concat()可以直接将值拼接到数组后面
+// Plan D: map()
+function count4(arr, item) {
+    var time = 0
+    //map()-->对数组中的每一项进行给定函数，
+    //返回每次函数调用的结果组成的数组；
+    arr.map(function (a) {
+        if (a === item) {
+            time++
+        }
+    })
+    return time
 }
