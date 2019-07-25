bc7975d30d2a64707287fc2b6f0282a1d8c31339
doubleyao5753@gmail.com <47850689+OneShot5753@users.noreply.github.com>
2019-07-25 23:26:17 +0800

    04 code

--- Code/temp.js
+++ Code/temp.js
@@ -1,13 +1,10 @@
-function getRepeat2(arr) {
-    arr.sort();
-    var res = [];
-    for(var i = 0;i < arr.length ; i++) {
-        if (arr[i] === arr[i+1] && arr[i] !== arr[i-1]){
-            newArr.push(arr[i])
+function noRepeat(arr){
+    var res = []
+    arr.sort()
+    for(var i=0;i<arr.length;i++){
+        if(arr[i] !== arr[i+1]){
+            res.push(arr[i])
         }
-        // if(arr[i] === arr[i+1] && res.indexOf(i) == -1){
-        //     res.push(arr[i])
-        // }
     }
     return res
 }
