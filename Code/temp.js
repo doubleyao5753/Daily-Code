5790ec9a705adebfe947812ae53b2484d0403fcc
doubleyao5753@gmail.com <47850689+OneShot5753@users.noreply.github.com>
2019-07-27 17:47:15 +0800

    day 5 code

--- Code/temp.js
+++ Code/temp.js
@@ -1,10 +1,16 @@
-function noRepeat(arr){
+function getIndex(arr,item) {
     var res = []
-    arr.sort()
-    for(var i=0;i<arr.length;i++){
-        if(arr[i] !== arr[i+1]){
-            res.push(arr[i])
-        }
-    }
+    arr.forEach(function(ele,index) {
+        // 意思是：只有ele等于item时才会执行后边的表达式
+        ele == item && res.push(index)
+    })
+    return res
+}
+// filter与foreach的区别在于，filter的函数需要return
+function getIndex(arr,item) {
+    var res = []
+    arr.filter(function(ele,index) {
+        return ele == item && res.push(index)
+    })
     return res
 }
