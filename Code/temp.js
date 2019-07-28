8209bddae0c781d0530720e84bd04716c3316f56
doubleyao5753@gmail.com <47850689+OneShot5753@users.noreply.github.com>
2019-07-28 23:31:41 +0800

    day 6

--- Code/temp.js
+++ Code/temp.js
@@ -1,16 +1,13 @@
-function getIndex(arr,item) {
-    var res = []
-    arr.forEach(function(ele,index) {
-        // 意思是：只有ele等于item时才会执行后边的表达式
-        ele == item && res.push(index)
-    })
-    return res
-}
-// filter与foreach的区别在于，filter的函数需要return
-function getIndex(arr,item) {
-    var res = []
-    arr.filter(function(ele,index) {
-        return ele == item && res.push(index)
-    })
-    return res
+// setTimeout 不推荐
+function count2(start , end) {
+    if (start <= end) {
+        console.log(start)
+        start++;
+        var timer = setTimeout(function(){count(start,end)},100)
+    }
+    return {
+        cancel: function(){
+            clearTimeout(timer)
+        }
+    }
 }
