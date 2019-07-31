84392b6fd01d028733d69c3d0c9483a901f3a55f
doubleyao5753@gmail.com <47850689+OneShot5753@users.noreply.github.com>
2019-07-31 23:56:52 +0800

    day 7

--- Code/temp.js
+++ Code/temp.js
@@ -1,13 +1,17 @@
-// setTimeout 不推荐
-function count2(start , end) {
-    if (start <= end) {
-        console.log(start)
-        start++;
-        var timer = setTimeout(function(){count(start,end)},100)
-    }
-    return {
-        cancel: function(){
-            clearTimeout(timer)
-        }
+// 使用变量暂存区交换的原理:
+// for循环遍历数组，定义两种索引：顺序和倒序
+// 将顺序的值赋给暂存区，倒序的值赋给正序，暂存区的值赋给倒序
+function reverseArr(arr) {
+    // 定义暂存变量
+    let temp
+    for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
     }
 }
+
+ // test===========
+ let some = [5,4,6,3,4,9]
+ reverseArr(some)
+ console.log (some)  // =>[9, 4, 3, 6, 4, 5]
